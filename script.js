0b86b97c756fd2712c871d04e045fc82004eb3e7
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-06 11:05:06 -0400

    Cleaned up header in CSS, added some text

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Placeholder of JS added content";
-document.querySelector("body").appendChild(h2);
+// const h2 = document.createElement("h2");
+// h2.textContent = "Placeholder of JS added content";
+// document.querySelector("body").appendChild(h2);
