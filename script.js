2864e6ab69d957beadaeb479073facb250ade42a
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-08 18:09:04 -0400

    added a script to change background on hover

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 // const h2 = document.createElement("h2");
 // h2.textContent = "Placeholder of JS added content";
 // document.querySelector("body").appendChild(h2);
+
+const dead = document.getElementById("stealie");
+
+dead.addEventListener("mouseover", event => {
+    document.getElementById("body").style.backgroundImage = "url(images/dancingBears.jpg)";
+});
+
+// dead.addEventListener("mouseleave", event => {
+//     document.getElementById("body").style.backgroundColor = "rgb(212, 182, 199)";
+//   });
+
+//   setTimeout(function() {
+//     event.target.style.color = "";
+//   }, 500);
+// }, false);
