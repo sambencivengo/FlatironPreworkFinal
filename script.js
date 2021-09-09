e0948407f5c82f5459cd8788c23a1b472269570d
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-09 14:37:03 -0400

    Finished sidebar script, added bik picture and adjusted formating. Possible Final Commit

--- script.js
+++ script.js
@@ -4,15 +4,17 @@
 
 const dead = document.getElementById("stealie");
 
-dead.addEventListener("mouseover", event => {
-    document.getElementById("body").style.backgroundImage = "url(images/dancingBears.jpg)";
+const sidenav = document.getElementById("sidenav");
+//working hover script//
+dead.addEventListener("mouseenter", event => {
+    sidenav.style.backgroundImage = "url(/Users/sambencivengo/Development/code/Prework_Final_Project/images/f756cbde022ceaa11d1c4b14ee3f8aa7.jpg)";
 });
 
-// dead.addEventListener("mouseleave", event => {
-//     document.getElementById("body").style.backgroundColor = "rgb(212, 182, 199)";
-//   });
+dead.addEventListener("mouseleave", event => {
+    sidenav.style.backgroundImage = null;
+});
 
-//   setTimeout(function() {
-//     event.target.style.color = "";
-//   }, 500);
-// }, false);
+// if (event.type == 'mouseout') {
+//     event.target.style.background = ''
+//   }
+// }
