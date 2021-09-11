4cf4aede56cd63706bf0c3371b0bb4e13d3eee2c
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-11 11:33:29 -0400

    fixed javascript image pathing, sidebar issue should now be resolved.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const deadSkull = document.getElementById("stealie");
 const sidenav = document.getElementById("sidenav");
 //working hover script//
 deadSkull.addEventListener("mouseenter", event => {
-    sidenav.style.backgroundImage = "url(/Users/sambencivengo/Development/code/Prework_Final_Project/images/f756cbde022ceaa11d1c4b14ee3f8aa7.jpg)";
+    sidenav.style.backgroundImage = "url(images/bears.jpg)";
 });
 
 deadSkull.addEventListener("mouseleave", event => {
