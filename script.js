210b174fbd751badfd54a4440c3877e01ee91aaf
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-11 11:16:09 -0400

    Fixed javascript and prevented image distortion in the about page

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 // h2.textContent = "Placeholder of JS added content";
 // document.querySelector("body").appendChild(h2);
 
-const dead = document.getElementById("stealie");
+const deadSkull = document.getElementById("stealie");
 
 const sidenav = document.getElementById("sidenav");
 //working hover script//
-dead.addEventListener("mouseenter", event => {
+deadSkull.addEventListener("mouseenter", event => {
     sidenav.style.backgroundImage = "url(/Users/sambencivengo/Development/code/Prework_Final_Project/images/f756cbde022ceaa11d1c4b14ee3f8aa7.jpg)";
 });
 
-dead.addEventListener("mouseleave", event => {
+deadSkull.addEventListener("mouseleave", event => {
     sidenav.style.backgroundImage = null;
 });
 
